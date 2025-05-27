4513d3dcdbefc11f661741e2cf3e13724dc3043e
Harrish <harrish.wizinoa@gmail.com>
2025-05-27 16:02:55 +0530

    Deploy Flutter web to gh-pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0c678944e50a73233408e8b15dfc92cc
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "fe7342960ca5b0e655da7fdd392fb025",
+"flutter_bootstrap.js": "ff61ee11dd4a23eddcc4e226bf3b5323",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
